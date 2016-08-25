a72c2819fbbd23fe55ccd8a89425d339c67123b4
WataruMaeda <wm19850716@gmail.com>
2016-08-25 16:52:56 +0900

    タイマー機能追加

--- sampleTimerApp/ViewController.swift
+++ sampleTimerApp/ViewController.swift
@@ -8,18 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    var count = 0
+    var second = 0
+    var milliSecond = 0
+    var timer: NSTimer?
+    @IBOutlet var timerLabel: UILabel!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func updateTimerLabel()
+    {
+        count += 1
+        
+        // ミリ秒
+        milliSecond += 1
+        if milliSecond == 10 {
+            milliSecond = 0
+        }
+        
+        // 秒
+        second = count / 10
+        
+        // 0埋め
+        let mSec = String(format: "%02d", milliSecond)
+        let sec =  String(format: "%02d", second)
+        
+        
+        // タイマー表示
+        self.timerLabel!.text = sec + ":" + mSec
+    }
+    
+    @IBAction func startTimer(sender: AnyObject)
+    {
+        timer = nil
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: "updateTimerLabel", userInfo: nil, repeats: true)
     }
 
-    override func didReceiveMemoryWarning() {
+    @IBAction func stopTimer(sender: AnyObject)
+    {
+        timer?.invalidate()
+    }
+    
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
